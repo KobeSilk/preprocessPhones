61178ea28455dba1394429257e9931abab415ebc
KobeSilk <kobe.dillen@wearesilk.be>
2024-10-17 16:13:57 +0200

    Made it so invalid phone numbers will show "invalid".

--- preprocess_phones.py
+++ preprocess_phones.py
@@ -4,47 +4,38 @@ import streamlit as st
 import re
 
 st.title("Phone number cleaner")
-phonenumber = pd.read_csv(st.file_uploader("upload phone numbers",type="csv"))
+phonenumber = st.file_uploader("upload phone numbers",type="csv")
 if phonenumber is not None: 
-    column = st.selectbox("Choose column that has phone numbers:",phonenumber.columns)
-# Define the string
+    phonenumber = pd.read_csv(phonenumber)
+    column = st.selectbox("Choose column that has phone numbers:",phonenumber.columns,index=None)
+    if column is not None:
+        df = phonenumber
+        df[column] = df[column].astype(str)
+        df['mobile_cleaned'] = df[column].apply(lambda x: re.sub(r"[()\[\]{}<>].*?[()\[\]{}<>]|\.|\/|[a-zA-Z]| +", '', str(x)).strip())
+        df['mobile_cleaned'] = df['mobile_cleaned'].apply(lambda x: re.sub(r'[^\x20-\x7E]+', '', str(x)))
+        df.mobile_cleaned=np.where(df.mobile_cleaned.str.startswith('32'),'+'+ df.mobile_cleaned,df.mobile_cleaned)
+        df.mobile_cleaned=np.where(df.mobile_cleaned.str.startswith('00'),'+'+ 
+        df.mobile_cleaned.str[2:],df.mobile_cleaned)
+        df.mobile_cleaned=np.where(df.mobile_cleaned.str.startswith('0'),'+32'+ 
+        df.mobile_cleaned.str[1:],df.mobile_cleaned)
+        df.mobile_cleaned=np.where(df.mobile_cleaned.str.startswith('3'),'+32'+ 
+        df.mobile_cleaned.str[1:],df.mobile_cleaned)
+        df['mobile_cleaned'] = df['mobile_cleaned'].apply(lambda x: "invalid" if x.count('+') > 1 else x)
+        df['mobile_cleaned'] = df['mobile_cleaned'].apply(lambda x: "invalid" if len(str(x)) > 14 else x)
+        
+        @st.cache_data
+        def convert_df(df):
+            # IMPORTANT: Cache the conversion to prevent computation on every rerun
+            return df.to_csv().encode("utf-8")
+        st.dataframe(df)
+        df['mobile_cleaned'] = '="' + df['mobile_cleaned'].astype(str) + '"'
+        csv = convert_df(df)
 
-    df = phonenumber
-    df[column] = df[column].astype(str)
-    df['mobile_cleaned'] = df[column].apply(lambda x: re.sub(r"[()\[\]{}<>].*?[()\[\]{}<>]|\.|\/| +", '', str(x)).strip())
-    df.mobile_cleaned=np.where(df.mobile_cleaned.str.startswith('32'),'+'+ df.mobile_cleaned,df.mobile_cleaned)
-    df.mobile_cleaned=np.where(df.mobile_cleaned.str.startswith('00'),'+'+ 
-    df.mobile_cleaned.str[2:],df.mobile_cleaned)
-    df.mobile_cleaned=np.where(df.mobile_cleaned.str.startswith('0'),'+32'+ 
-    df.mobile_cleaned.str[1:],df.mobile_cleaned)
-    df.mobile_cleaned=np.where(df.mobile_cleaned.str.startswith('3'),'+32'+ 
-    df.mobile_cleaned.str[1:],df.mobile_cleaned)
-    #df.mobile_cleaned=np.where(df.mobile_cleaned.str.startswith('04'),'+32'+ 
-    #df.mobile_cleaned.str[1:],df.mobile_cleaned)
-    #df.mobile_cleaned=np.where(df.mobile_cleaned.str.startswith('03'),'+32'+ 
-    #df.mobile_cleaned.str[1:],df.mobile_cleaned)
-    #df.mobile_cleaned=np.where(df.mobile_cleaned.str.startswith('08'),'+32'+ 
-    #df.mobile_cleaned.str[1:],df.mobile_cleaned)
-    #df.mobile_cleaned=np.where(df.mobile_cleaned.str.startswith('02'),'+32'+   
-    #df.mobile_cleaned.str[1:],df.mobile_cleaned)
-    #df.mobile_cleaned=np.where(df.mobile_cleaned.str.startswith('09'),'+32'+ 
-    #df.mobile_cleaned.str[1:],df.mobile_cleaned)
-    #df.mobile_cleaned=np.where(df.mobile_cleaned.str.startswith('05'),'+32'+ 
-    #df.mobile_cleaned.str[1:],df.mobile_cleaned)
-#
-    df.mobile_cleaned = df.mobile_cleaned.str.replace(' ', '')
-    @st.cache_data
-    def convert_df(df):
-        # IMPORTANT: Cache the conversion to prevent computation on every rerun
-        return df.to_csv().encode("utf-8")
-    st.table(df)
-    df['mobile_cleaned'] = '="' + df['mobile_cleaned'].astype(str) + '"'
-    csv = convert_df(df)
-    df.to_clipboard(excel=True)
-
-    st.download_button(
-        label="Download data as CSV",
-        data=csv,
-        file_name="phones_cleaned.csv",
-        mime="text/csv",
-    )
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name="phones_cleaned.csv",
+            mime="text/csv",
+        )
+else:
+    st.error("Please upload a file.")
